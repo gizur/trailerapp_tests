a5b84061f1bbe289080b2189fc36db553d3787ff
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2016-03-08 17:21:35 +0100

    Working on tests

--- test.js
+++ test.js
@@ -13,17 +13,17 @@ chaiAsPromised.transferPromiseness = wd.transferPromiseness;
 var debug = console.log.bind(console, 'DEBUG');
 
 var serverConfigs = {
-    local: {
-      host: 'localhost',
-      port: 4723
-    },
-    sauce: {
-      host: 'ondemand.saucelabs.com',
-      port: 80,
-      username: process.env.SAUCE_USERNAME,
-      password: process.env.SAUCE_ACCESS_KEY
-    }
-  };
+  local: {
+    host: 'localhost',
+    port: 4723
+  },
+  sauce: {
+    host: 'ondemand.saucelabs.com',
+    port: 80,
+    username: process.env.SAUCE_USERNAME,
+    password: process.env.SAUCE_ACCESS_KEY
+  }
+};
 
 var configure = function (driver) {
   // See whats going on
@@ -36,6 +36,11 @@ var configure = function (driver) {
   driver.on('http', function (meth, path, data) {
     console.log(' > ' + meth.magenta, path, (data || '').grey);
   });
+};
+
+// return false for null and undefined and true for everything else
+var notNull = function (msg, x) {
+  if (!x) throw new Error(msg)
 };
 
 describe("trailerapp tests", function () {
@@ -58,7 +63,19 @@ describe("trailerapp tests", function () {
       app: 'Besiktning/platforms/ios/build/emulator/Besiktning.app'
     };
 
-    return driver.init(desired);
+    return driver.init(desired)
+      .elementsByIosUIAutomation('.tableViews()[0].cells()', function (err, el) {
+        el.elementByIosUIAutomation('.elements()["UICatalog"]', function (err, el) {
+          el.getAttribute('name', function (err, name) {
+            console.log(name);
+          });
+        });
+      })
+      .contexts().then(function (contexts) {
+        // get list of available views. Returns array: ["NATIVE_APP","WEBVIEW_1"]
+        debug(contexts);
+        return driver.context(contexts[1]); // choose the webview context
+      })
   });
 
   after(function () {
@@ -76,19 +93,18 @@ describe("trailerapp tests", function () {
   });
 
 
-  it("should get the url", function () {
-
-    // get list of available views. Returns array: ["NATIVE_APP","WEBVIEW_1"]
+  it("should display the content page with three tabs", function () {
     return driver
-      .contexts().then(function (contexts) {
-        debug(contexts);
-        return driver.context(contexts[1]); // choose the webview context
-      })
-      .eval('document.getElementById("deviceready")')
-      .then(function (res) {
-        debug(res);
-      });
-    
+      .elementById('contacts').then(notNull.bind(this, 'missing contacts tab'))
+      .elementById('survey').then(notNull.bind(this, 'missing survey tab'))
+      .elementById('settings').then(notNull.bind(this, 'missing settings tab'));
+  });
+
+  it("should should be albe to login", function () {
+    return driver
+      .elementById('contacts').then(notNull.bind(this, 'missing contacts tab'))
+      .elementById('survey').then(notNull.bind(this, 'missing survey tab'))
+      .elementById('settings').then(notNull.bind(this, 'missing settings tab'));
   });
 
 });
